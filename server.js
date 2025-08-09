1453151597aba9c7f9de23451b109b037f76d7aa
Lee Hye Won <thankyou@sookmyung.ac.kr>
2025-08-09 19:40:00 +0900

    setup: 폴더 구조 구성 및 DB 연결 환경 설정

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
-const express = require("express");
-const mongoose = require("mongoose");
 require("dotenv").config();
 
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./src/config/db");
+
 const app = express();
-app.use(express.json());
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB 연결 완료!"))
-  .catch((err) => console.error(err));
+app.use(cors()); // 다른 도메인 요청 허용
+app.use(express.json()); // JSON 형식 body 파싱
 
+//DB 연결 불러오기
+connectDB();
+
+// 기본 라우트
 app.get("/", (req, res) => {
   res.send("백엔드 시작!");
 });
 
-app.listen(3000, () => {
-  console.log("서버 시작: http://localhost:3000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`서버 시작: http://localhost:${PORT}`);
 });
