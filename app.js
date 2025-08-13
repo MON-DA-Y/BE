6b0c96d01051c5750705aab7cd59db461896b88e
Lee Hye Won <thankyou@sookmyung.ac.kr>
2025-08-14 01:08:01 +0900

    feat/#1: attendance API 연결

--- app.js
+++ app.js
@@ -5,17 +5,25 @@ const cors = require("cors");
 const connectDB = require("./src/config/db");
 
 const app = express();
+const attendanceRouter = require("./src/routes/attendanceRouter");
 
-app.use(cors()); // 다른 도메인 요청 허용
+app.use(
+  cors({
+    origin: "http://localhost:3000", // 프론트 포트
+  })
+); // 다른 도메인 요청 허용
 app.use(express.json()); // JSON 형식 body 파싱
 
-//DB 연결 불러오기
+// DB 연결 불러오기
 connectDB();
 
 // 기본 라우트
 app.get("/", (req, res) => {
   res.send("백엔드 시작!");
 });
+
+// 출석률 조회 라우터
+app.use("/api/users", attendanceRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
