0fd6ea9b4dc5061891651efc01948b5eccc812e9
You Dongeun <130419669+uehddms@users.noreply.github.com>
2025-09-08 23:39:43 +0900

    feat/#25: monQuiz 제출 router 설정

--- src/routes/monQuizRouter.js
+++ src/routes/monQuizRouter.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { getTodayMonQuiz } = require("../controllers/monQuizController");
+const {
+  getTodayMonQuiz,
+  postMonQuizSubmit,
+} = require("../controllers/monQuizController");
 
 // 오늘 mon퀴즈 조회
 router.get("/monQuiz", getTodayMonQuiz);
+
+// 오늘 mon퀴즈 제출
+router.get("/monQuiz/submit", postMonQuizSubmit);
 
 module.exports = router;
