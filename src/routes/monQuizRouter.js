03b08f7a8352fcbed612c3e77561f1169898b037
You Dongeun <130419669+uehddms@users.noreply.github.com>
2025-09-15 23:53:54 +0900

    feat/#25: monQuiz 채점 조회 라우터 구현

--- src/routes/monQuizRouter.js
+++ src/routes/monQuizRouter.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const {
   getTodayMonQuiz,
   postMonQuizSubmit,
+  getTodayMonQuizMark,
 } = require("../controllers/monQuizController");
 
 // 오늘 mon퀴즈 조회
@@ -10,5 +11,8 @@ router.get("/monQuiz", getTodayMonQuiz);
 
 // 오늘 mon퀴즈 제출
 router.post("/monQuiz/submit", postMonQuizSubmit);
+
+// mon퀴즈 채점 조회
+router.get("/monQuiz/mark", getTodayMonQuizMark);
 
 module.exports = router;
