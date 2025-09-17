54894396c72f6bba61dd1ee721c02088ff81d5b4
Lee Hye Won <thankyou@sookmyung.ac.kr>
2025-09-17 15:31:44 +0900

    feat/#44: 학생-학부모 연결

--- src/routes/studentInfoRouter.js
+++ src/routes/studentInfoRouter.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { getStudentInfo } = require("../controllers/studentInfoController");
+const { getStudentInfo, getStudentByEmail } = require("../controllers/studentInfoController");
 
 // 학생 정보 조회
 router.get("/stdInfo", getStudentInfo);
+// 학생 - 학부모 연결
+router.get("/student/email", getStudentByEmail);
 
 module.exports = router;
