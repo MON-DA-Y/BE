4d36cdccb77cfede30cb683277ac5654a242c221
You Dongeun <130419669+uehddms@users.noreply.github.com>
2025-09-22 03:02:59 +0900

    feat/#67: monQuiz Router에 monQuiz 배정 API 구현

--- src/routes/monQuizRouter.js
+++ src/routes/monQuizRouter.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const {
+  assignMonQuizToStudent,
   getTodayMonQuiz,
   postMonQuizSubmit,
   getTodayMonQuizMark,
   postTodayMonQuizMarkDone,
   getStudentSubmit,
 } = require("../controllers/monQuizController");
+
+// 오늘 퀴즈 배정
+router.get("/monQuiz/assign", assignMonQuizToStudent);
 
 // 오늘 mon퀴즈 조회
 router.get("/monQuiz", getTodayMonQuiz);
