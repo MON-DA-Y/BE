d5116db93b6c9128f072a3c827ae5bb7648d54ac
Lee Hye Won <thankyou@sookmyung.ac.kr>
2025-09-23 03:18:29 +0900

    feat/#75: 저장된 studentId로 학생 히스토리 조회

--- src/routes/newsHistoryRouter.js
+++ src/routes/newsHistoryRouter.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { getNewsHistory } = require("../controllers/newsHistoryController");
+const { getNewsHistory, getParentNewsHistory } = require("../controllers/newsHistoryController");
 
 router.get("/history/news", getNewsHistory);
+router.get("/parent/history/news", getParentNewsHistory);
 
 module.exports = router;
