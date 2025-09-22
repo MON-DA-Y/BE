93e21a1179c01e135af0a94159736d0ff6f43509
You Dongeun <130419669+uehddms@users.noreply.github.com>
2025-09-23 03:32:39 +0900

    feat/#73: 특정 시리즈의 특정 파트 조회 Router 설정

--- src/routes/monSeriesRouter.js
+++ src/routes/monSeriesRouter.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const {
   assignSeriesPartToStudent,
   getAllSeries,
+  getSeriesPart,
 } = require("../controllers/monSeriesController");
 
 // 동기화된 시리즈 전체 조회
@@ -10,5 +11,8 @@ router.get("/", getAllSeries);
 
 // monSeries 배정 (파트 학습 완료 시)
 router.post("/assign", assignSeriesPartToStudent);
+
+// 특정 시리즈의 특정 파트 조회
+router.get("/:seriesId/parts/:partId", getSeriesPart);
 
 module.exports = router;
