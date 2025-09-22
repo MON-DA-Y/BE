2fb6953d3e214a1d02a197031a8a60f68ac338da
You Dongeun <130419669+uehddms@users.noreply.github.com>
2025-09-22 12:29:58 +0900

    feat/#67: level  라벨 util 생성

---
+++ src/utils/level.js
@@ -0,0 +1,20 @@
+export const getLevelLabel = (level) => {
+  switch (level) {
+    case 1:
+      return "씨앗";
+    case 2:
+      return "새싹";
+    case 3:
+      return "잎새";
+    case 4:
+      return "가지";
+    case 5:
+      return "나무";
+    case 6:
+      return "꽃";
+    case 7:
+      return "열매";
+    default:
+      return "씨앗";
+  }
+};
